d02602abcdea753e0e32c259230c37f859f28748
Lincoln Ferreira Campos <link7-7@hotmail.com>
2019-12-30 04:37:48 -0300

    alternativas de escrever diretiva sem o *

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
-    DiretivaNgswitchComponent
+    DiretivaNgswitchComponent,
+    DiretivaNgforComponent
   ],
   imports: [
     BrowserModule,
---
+++ diretivas/src/app/diretiva-ngfor/diretiva-ngfor.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngfor',
+  templateUrl: './diretiva-ngfor.component.html',
+  styleUrls: ['./diretiva-ngfor.component.css']
+})
+export class DiretivaNgforComponent implements OnInit {
+
+  cursos: string[] = ["Angular", "C#", "PHP"];
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
