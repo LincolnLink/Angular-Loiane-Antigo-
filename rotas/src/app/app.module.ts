d0f52bf9fd78c80b0fff15a8cca17cdc4f315234
LincolnLink <link7-7@hotmail.com>
2020-01-27 01:50:28 -0300

    iniciando estudo de rotas

---
+++ rotas/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MaterializeModule } from 'angular2-materialize';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterializeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
