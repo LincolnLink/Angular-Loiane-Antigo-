9bc45ec23355bf9851aa8f58905a6f806718bb27
LincolnLink <link7-7@hotmail.com>
2020-01-28 15:37:38 -0300

    angular2 materilize foi descontinuado

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterializeModule } from 'angular2-materialize';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MaterializeModule
+    AppRoutingModule    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
