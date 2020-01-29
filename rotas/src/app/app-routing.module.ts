11918d727010fa1d30cbbc86bcc5e3af7aa8831c
LincolnLink <link7-7@hotmail.com>
2020-01-29 02:31:42 -0300

    iniciando configuração de rotas

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cursos', component:CursosComponent },
+  { path: 'login', component: LoginComponent},
+  { path: '', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
