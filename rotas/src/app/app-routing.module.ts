682afbbe4286206507f1ae498d5e657b3e3a235b
LincolnLink <link7-7@hotmail.com>
2020-01-31 20:05:54 -0300

    instalando NGX-BOOTSTRAP

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
 import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+
 
 
 const routes: Routes = [
--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { NgxNavbarModule } from 'ngx-bootstrap-navbar'; 
+
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
@@ -17,8 +21,11 @@ import { CursosComponent } from './cursos/cursos.component';
     CursosComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule    
+    AppRoutingModule,  
+    BrowserAnimationsModule,  
+    NgxNavbarModule
     
   ],
   providers: [],
