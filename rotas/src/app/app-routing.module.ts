820c731c078fde008656b918719acdf997a810fe
LincolnLink <link7-7@hotmail.com>
2020-01-31 21:54:24 -0300

    aplicando style quando a rota é ativada

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 
-
+/* Define o nome da rota e vincula um componente */
 const routes: Routes = [
   { path: 'cursos', component:CursosComponent },
   { path: 'login', component: LoginComponent},
   { path: '', component: HomeComponent}
 ];
 
+/*Configura a lista de rotas que foi definidas! */
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
