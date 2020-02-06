ef1bac67dd3bf69c29c32c92f2d02e3c3c474af3
LincolnLink <link7-7@hotmail.com>
2020-02-06 04:35:29 -0300

    separando modulo de modulo de rotas

--- rotas/src/app/cursos/cursos.module.ts
+++ rotas/src/app/cursos/cursos.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from '../app-routing.module';
 
 import { CursosComponent } from './cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursosService } from './cursos.service';
-
+import { CursosRoutingModule } from './cursos-routing.module';
 
 
 @NgModule({
@@ -17,9 +15,8 @@ import { CursosService } from './cursos.service';
     CursoNaoEncontradoComponent
   ],
   imports: [
-    CommonModule,
-    FormsModule,
-    AppRoutingModule
+    CommonModule,    
+    CursosRoutingModule
   ],
   providers: [CursosService]
 })
